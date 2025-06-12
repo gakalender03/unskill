eee46d06d3f5803798f1d0ea41467b179c37be3f
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-06-12 15:32:10 +0700

    Update paralelfix.js

--- paralelfix.js
+++ paralelfix.js
@@ -9,9 +9,9 @@ const CONFIG = {
   GAS_LIMIT: 300000,
   GAS_PRICE: ethers.parseUnits('0.000000003', 'ether'),
   EXPLORER_URL: 'https://seitrace.com',
-  BATCH_SIZE: 50,
+  BATCH_SIZE: 5,
   TOTAL_TX: 1000,
-  DELAY_BETWEEN_BATCHES: 10000,
+  DELAY_BETWEEN_BATCHES: 1000,
   AMOUNT_TO_BRIDGE: '0.000001',
 };
 
